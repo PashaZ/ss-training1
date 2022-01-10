d7263adde0abb006059d2ce92f73d1fd21c57b97
Pasha <pavlo.zakernychnyy@gmail.com>
2022-01-10 14:54:29 +0200

    created_3_pages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Ccomponent from './Ccomponent';
+
+function App() {
+    return (
+        <div>
+            <Ccomponent />
+        </div>
+    )
+};
+export default App;
