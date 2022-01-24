49674a96b6acb5613d5311fd0cb93a5c91c0d528
Pasha <pavlo.zakernychnyy@gmail.com>
2022-01-24 12:11:03 +0200

    test simple server

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+// const PORT = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!') 
+});
+app.get('/about', (req, res)=>{
+    res.send('Hello About')
+});
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
